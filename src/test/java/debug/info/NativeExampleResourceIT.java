a61ed6844763fce90bc0855806be5eb2c758ec35
Galder Zamarreño <galder@zamarreno.com>
2020-06-02 08:05:09 +0200

    Initial skeleton

---
+++ src/test/java/debug/info/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package debug.info;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
